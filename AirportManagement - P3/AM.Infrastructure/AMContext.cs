f09506233eba44dac421f3c82c8ff49b3bb91d8d
H4mzaDevs <hamza.ghariani@esprit.tn>
2023-02-25 16:01:54 +0100

    TP3

---
+++ AirportManagement - P3/AM.Infrastructure/AMContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AM.Infrastructure
+{
+    public class AMContext  : DbContext
+    {
+        //public DbSet<Flight> Customers { get; set; }//
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
